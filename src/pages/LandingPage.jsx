fe557a20b71008345b3d9de8c47d0dea3407d223
samueljsong <semuall0108@gmail.com>
2025-03-18 07:13:17 -0700

    changed landing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -32,7 +32,7 @@ export const LandingPage = () => {
                             Immanuel Lutheran Church
                         </h1>
                         <h1 className=" lp-title">
-                            Together in Faith United in Love.
+                            Together in Faith United in God's Love.
                         </h1>
                     </div>
 
